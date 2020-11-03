c09bc9008e326098f7988bd57d02ae210eebaee7
Jeune Quentin <72392037+CaesarDeVille@users.noreply.github.com>
2020-11-03 20:39:54 +0100

    Add files via upload

--- js/pong.js
+++ js/pong.js
@@ -1,11 +1,14 @@
-let largeur=$("#balle").width();
-let gauche=parseInt($("#balle").css("left"));
-let haut=parseInt($("#balle").css("top"));
-alert(gauche);
+//Déclaration de variables
+let largeur = $("#balle").width();
+let gauche = parseInt($("#balle").css("left"));
+let haut = parseInt($("#balle").css("top"));
+//alert(gauche);
 
-setInterval(function(){
-    gauche=gauche+1;
-    haut=haut+0.5;
+//boucle afin de modifier la position de la balle toutes les 10 millisecondes
+setInterval(function()
+{
+    gauche = gauche+1;
+    haut = haut+0.5;
     $("#balle").css("left",gauche);
     $("#balle").css("top",haut);
 }, 10);
